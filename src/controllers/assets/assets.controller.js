9457a082726c6262c83a77266aa5c8bde2627305
whoant <vovanhoangtuan4.2@gmail.com>
2022-12-11 14:20:30 +0700

    Add deposit and withdraw fund

--- src/controllers/assets/assets.controller.js
+++ src/controllers/assets/assets.controller.js
@@ -1,11 +1,32 @@
 const catchAsync = require('../../helpers/catchAsync.helper');
 const db = require('../../database');
 const { successResponse } = require('../../helpers/response.helper');
+
 exports.getAssets = catchAsync(async(req, res, next) => {
     const { user } = res.locals;
     const { rows } = await db.query(`select * from fn_get_all_assets('${user.user_id}');`);
 
     successResponse(req, res, {
         assets: rows,
+    });
+});
+
+exports.depositAsset = catchAsync(async(req, res, next) => {
+    const { amount } = req.body;
+    const { user } = res.locals;
+    await db.query(`call deposit_fund('${user.user_id}', ${amount}, 'stock_1');`);
+
+    successResponse(req, res, {
+        message: 'Nạp tiền thành công'
+    });
+});
+
+exports.withdrawAsset = catchAsync(async(req, res, next) => {
+    const { amount } = req.body;
+    const { user } = res.locals;
+    await db.query(`call withdraw_fund('${user.user_id}', ${amount}, 'stock_1');`);
+
+    successResponse(req, res, {
+        message: 'Rút tiền thành công'
     });
 });
--- src/routes/assets.route.js
+++ src/routes/assets.route.js
@@ -7,4 +7,8 @@ const authMiddleware = require('../middleware/auth.middleware');
 
 router.get('/', authMiddleware.selectUser('USER'), assetsController.getAssets);
 
+router.post('/deposit', authMiddleware.selectUser('USER'), assetsController.depositAsset);
+
+router.post('/withdraw', authMiddleware.selectUser('USER'), assetsController.withdrawAsset);
+
 module.exports = router;
