945e8389605bb81bedc00ceb25f7d7aab88e62f9
whoant <vovanhoangtuan4.2@gmail.com>
2022-12-11 13:58:45 +0700

    Add module tracking

--- src/loaders/expressLoader.js
+++ src/loaders/expressLoader.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const errorHandler = require('../middleware/errorHandler.middleware');
 const routes = require('../routes');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 module.exports = app => {
     app.use(express.json());
+    app.use(helmet());
+    app.use(morgan('combined'))
     routes(app);
     app.use(errorHandler);
 };
